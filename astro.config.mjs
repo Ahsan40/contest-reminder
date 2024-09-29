82b3e19e33e40622169a339af797a46b6349eb08
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-09-29 20:55:34 +0600

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,11 @@ import tailwind from "@astrojs/tailwind";
 
 import solidJs from "@astrojs/solid-js";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), tailwind(), solidJs()],
   output: "hybrid",
+  adapter: vercel(),
 });
